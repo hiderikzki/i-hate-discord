f99a3e34b181951a5651da616143d4f25b0055a0
BuildTools <foreheadchann@googlemail.com>
2023-12-07 18:34:44 +0200

    fuck it no auto close

--- redirector.js
+++ redirector.js
@@ -1,13 +1,8 @@
 const urlParams = new URLSearchParams(window.location.search);
 const text = document.getElementById('trole-text');
 
-function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
-}
-
 if (urlParams.has('map_id')) {
-    open("osu://b/" + urlParams.get('map_id'));
-    delay(1000).then(() => window.close());
+    window.location.replace("osu://b/" + urlParams.get('map_id'));
 } else {
     text.innerHTML = "provide map_id as url parameter, k thanks try again!!!";
 }
