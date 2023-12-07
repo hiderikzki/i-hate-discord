4306561c9fab7c1d0963e4cd1d39ed93a8e89868
BuildTools <foreheadchann@googlemail.com>
2023-12-07 18:22:22 +0200

    testing autoclose

--- redirector.js
+++ redirector.js
@@ -3,6 +3,7 @@ const text = document.getElementById('trole-text');
 
 if (urlParams.has('map_id')) {
     window.location.replace("osu://b/" + urlParams.get('map_id'));
+    window.close();
 } else {
     text.innerHTML = "provide map_id as url parameter, k thanks try again!!!";
 }
