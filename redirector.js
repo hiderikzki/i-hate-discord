a546386021a4d58a105f9e30a7763ba1de97da6c
BuildTools <foreheadchann@googlemail.com>
2023-12-07 18:57:18 +0200

    testing for the 789125678125th time

--- redirector.js
+++ redirector.js
@@ -1,23 +1,19 @@
 const urlParams = new URLSearchParams(window.location.search);
 const text = document.getElementById('trole-text');
 
+/*
 const delay = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
-  };
+};
+*/
 
 if (urlParams.has('map_id')) {
-    window.open("osu://b/" + urlParams.get('map_id'), "_blank");
+    const url = "osu://b/" + urlParams.get('map_id');
+    const open_page = confirm('Open The Url? (' + url + ')');
 
-    delay(1000);
-    
-    if (window.localStorage.getItem("autoclose") === true) {
+    if (open_page) {
+        window.open(url, "_blank");
         window.close();
-    } else {
-        const autoclose = confirm('Allow Automatic Closing? (only allow if set application open to always allow)');
-
-        if (autoclose) {
-            window.localStorage.setItem("autoclose", true);
-        }
     }
 } else {
     text.innerHTML = "provide map_id as url parameter, k thanks try again!!!";
