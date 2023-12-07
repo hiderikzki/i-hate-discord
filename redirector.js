a72e9d3b411ab769775e491f744925c12bb4318a
BuildTools <foreheadchann@googlemail.com>
2023-12-07 18:32:24 +0200

    aaaaaaaaaaa

--- redirector.js
+++ redirector.js
@@ -6,7 +6,7 @@ function delay(time) {
 }
 
 if (urlParams.has('map_id')) {
-    window.location.replace("osu://b/" + urlParams.get('map_id'));
+    open("osu://b/" + urlParams.get('map_id'));
     delay(1000).then(() => window.close());
 } else {
     text.innerHTML = "provide map_id as url parameter, k thanks try again!!!";
