a477ec2b8a11fb610a68b5dcf6b03a826270888e
BuildTools <foreheadchann@googlemail.com>
2023-12-07 18:43:54 +0200

    testing another autoclose approach

--- redirector.js
+++ redirector.js
@@ -1,8 +1,23 @@
 const urlParams = new URLSearchParams(window.location.search);
 const text = document.getElementById('trole-text');
 
+const delay = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  };
+
 if (urlParams.has('map_id')) {
     window.location.replace("osu://b/" + urlParams.get('map_id'));
+    
+    delay(1000);
+    
+    if (window.localStorage.getItem("autoclose")) {
+        window.close();
+    } else {
+        if (autoclose) {
+            autoclose = confirm('Allow Automatic Closing? (only allow if set application open to always allow)');
+            window.localStorage.setItem("autoclose", true);
+        }
+    }
 } else {
     text.innerHTML = "provide map_id as url parameter, k thanks try again!!!";
 }
