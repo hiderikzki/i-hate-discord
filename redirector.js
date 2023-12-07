d3bb9293781cb49e4782564f93aaff531dbc2c8f
BuildTools <foreheadchann@googlemail.com>
2023-12-07 18:26:41 +0200

    testing again

--- redirector.js
+++ redirector.js
@@ -1,9 +1,13 @@
 const urlParams = new URLSearchParams(window.location.search);
 const text = document.getElementById('trole-text');
 
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
 if (urlParams.has('map_id')) {
     window.location.replace("osu://b/" + urlParams.get('map_id'));
-    window.close();
+    delay(1000).then(() => window.close());
 } else {
     text.innerHTML = "provide map_id as url parameter, k thanks try again!!!";
 }
